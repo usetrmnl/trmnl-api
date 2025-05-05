b02c2ccbe92af552b45e4df3036a79b7c6f2e03c
Brooke Kuhlmann <brooke@alchemists.io>
2025-05-05 16:19:15 -0600

    Added endpoints container
    Necessary to register all endpoints for eventual injection into to the client.
    
    Milestone: minor

---
+++ lib/trmnl/api/endpoints/container.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "containable"
+
+module TRMNL
+  module API
+    module Endpoints
+      # Registers all endpoints.
+      module Container
+        extend Containable
+
+        register(:current_screen) { CurrentScreen.new }
+        register(:display) { Display.new }
+        register(:firmware) { Firmware.new }
+        register(:log) { Log.new }
+        register(:setup) { Setup.new }
+      end
+    end
+  end
+end
