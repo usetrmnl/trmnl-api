725cae40eeeaa5adc225860f904964bdb4969c16
Brooke Kuhlmann <brooke@alchemists.io>
2025-05-12 13:47:51 -0600

    Added RSpec shared endpoint examples
    Necessary to a define shared example for all endpoints to reduce duplication. This'll soon be used in all endpoint specs.
    
    Milestone: minor

---
+++ spec/support/shared_examples/an_inspectable_endpoint.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples "an inspectable endpoint" do |target|
+  it "answers contract class" do
+    expect(target.inspect).to include("@contract=Dry::Schema::JSON, ")
+  end
+end
