00dec41b16363d837521c714d4ca4610ea18cd57
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-18 13:09:40 -0600

    Added model
    Necessary to provide a whole value object as the output from the API response.
    
    Milestone: minor

---
+++ lib/trmnl/api/models/model.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module TRMNL
+  module API
+    module Models
+      # Models data for API display responses.
+      Model = Struct.new(
+        :name,
+        :label,
+        :description,
+        :colors,
+        :bit_depth,
+        :scale_factor,
+        :rotation,
+        :mime_type,
+        :width,
+        :height,
+        :offset_x,
+        :offset_y,
+        :published_at
+      ) do
+        def self.for(attributes) = new(**attributes)
+
+        def initialize(**)
+          super
+          apply_defaults
+          freeze
+        end
+
+        def to_json(*) = to_h.to_json(*)
+
+        private
+
+        def apply_defaults
+          %i[colors bit_depth scale_factor rotation width height offset_x offset_y].each do |name|
+            self[name] ||= 0
+          end
+        end
+      end
+    end
+  end
+end
