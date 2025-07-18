0eb02b536030f01d18c283f3e7e77c9f20db593b
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-18 12:56:48 -0600

    Added model contract
    Necessary to validate the HTTP response from the Core server.
    
    Milestone: minor

---
+++ lib/trmnl/api/contracts/model.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "dry/schema"
+
+module TRMNL
+  module API
+    module Contracts
+      # Validates API response.
+      Model = Dry::Schema.JSON do
+        required(:name).filled :string
+        required(:label).filled :string
+        required(:description).filled :string
+        required(:colors).filled :integer
+        required(:bit_depth).filled :integer
+        required(:scale_factor).filled :integer
+        required(:rotation).filled :integer
+        required(:mime_type).filled :string
+        required(:width).filled :integer
+        required(:height).filled :integer
+        required(:offset_x).filled :integer
+        required(:offset_y).filled :integer
+        required(:published_at).filled :time
+      end
+    end
+  end
+end
