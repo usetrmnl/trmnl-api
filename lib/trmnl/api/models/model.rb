21c0cc86be8f76a1c76e21a55ac913b52486d4dd
Brooke Kuhlmann <brooke@alchemists.io>
2025-08-06 11:13:55 -0600

    Removed model endpoint notes
    Accidentially forgot to remove these engineering notes during the original implementation work. They are not needed.
    
    Milestone: patch

--- lib/trmnl/api/models/model.rb
+++ lib/trmnl/api/models/model.rb
@@ -40,28 +40,3 @@ module TRMNL
     end
   end
 end
-
-# TODO: Remove when finished.
-__END__
-
-# frozen_string_literal: true
-
-module Kagi
-  module API
-    module Models
-      # Models the search payload.
-      Search = Data.define :meta, :data do
-        def self.for(**attributes)
-          new(
-            **attributes.merge!(
-              meta: Content::Meta.for(**attributes[:meta]),
-              data: attributes[:data].map { Content::Search.for(**it) }
-            )
-          )
-        end
-      end
-    end
-  end
-end
-
-
