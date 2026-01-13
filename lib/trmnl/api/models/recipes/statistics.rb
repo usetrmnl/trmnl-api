097d77369d9c7ffca4b56b6fddffa1380095e124
Brooke Kuhlmann <brooke@alchemists.io>
2026-01-13 15:04:27 -0700

    Added recipe statistics model
    Necessary to model the statistics of the response. Will soon be used by the recipe model (soon to be used).
    
    Milestone: minor

---
+++ lib/trmnl/api/models/recipes/statistics.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module TRMNL
+  module API
+    module Models
+      module Recipes
+        # Models the statistics of the API response.
+        Statistics = Data.define :installs, :forks
+      end
+    end
+  end
+end
